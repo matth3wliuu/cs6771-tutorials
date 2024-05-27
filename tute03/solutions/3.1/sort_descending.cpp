12b0fbc6dd494f7394ccb2e9885575b862d425e5
Matthew Liu <mattliurw@gmail.com>
2024-05-27 17:10:43 +0200

    init

--- tute03/solutions/3.1/sort_descending.cpp
+++ tute03/solutions/3.1/sort_descending.cpp
@@ -3,6 +3,8 @@
 #include <functional>
 
 auto sort_descending(std::vector<int>& numbers) -> void {
-    // why write any custom code when the standard library has your back?
-    std::sort(numbers.begin(), numbers.end(), std::greater<>{});
+	// why write any custom code when the standard library has your back?
+	std::sort(numbers.begin(), numbers.end(), std::greater<>{});
 }
+
+hello
