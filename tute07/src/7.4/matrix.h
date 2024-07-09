dd5fdce640d0ae664079161cbcfa933d289907f7
Matthew Liu <mattliurw@gmail.com>
2024-07-09 15:25:22 +1000

    tute07

---
+++ tute07/src/7.4/matrix.h
@@ -0,0 +1,8 @@
+#ifndef COMP6771_MATRIX_H
+#define COMP6771_MATRIX_H
+
+class matrix {
+
+};
+
+#endif // COMP6771_MATRIX_H
---
+++ tute07/src/7.4/matrix.test.cpp
@@ -0,0 +1,7 @@
+#include "./matrix.h"
+
+#include <catch2/catch.hpp>
+
+TEST_CASE("red pill or the blue pill") {
+    REQUIRE(false);
+}
